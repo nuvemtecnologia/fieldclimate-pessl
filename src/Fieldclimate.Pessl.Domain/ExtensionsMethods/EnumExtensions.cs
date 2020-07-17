15636b9c04fe91e153902c36baf0a73a888cf788
Elvis Fabian <elvis.fabian@nuvem.net>
2020-07-17 08:54:13 -0400

    Remove unused dependencies

--- src/Fieldclimate.Pessl.Domain/ExtensionsMethods/EnumExtensions.cs
+++ src/Fieldclimate.Pessl.Domain/ExtensionsMethods/EnumExtensions.cs
@@ -1,6 +1,5 @@
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Reflection;
 
@@ -16,25 +15,12 @@ namespace System
                 .First()
                 .GetCustomAttribute<TAttribute>();
         }
-
-        public static string GetDisplayName(this Enum enumvValue)
-        {
-            return enumvValue?.GetAttribute<DisplayAttribute>()?.Name ?? enumvValue?.ToString() ?? string.Empty;
-        }
-
+        
         public static string GetDescription(this Enum enumvValue)
         {
             return enumvValue?.GetAttribute<DescriptionAttribute>()?.Description ?? enumvValue?.ToString() ?? string.Empty;
         }
-
-        public static string GetValidOptionsAsString<T>() where T : struct
-        {
-            var allOptions = Array.AsReadOnly((T[]) Enum.GetValues(typeof(T)))
-                .Select(v => v.ToString());
-
-            return string.Join(", ", allOptions);
-        }
-
+        
         public static ReadOnlyCollection<T> GetValidOptions<T>() where T : struct
         {
             var allOptions = Array.AsReadOnly((T[]) Enum.GetValues(typeof(T)));
